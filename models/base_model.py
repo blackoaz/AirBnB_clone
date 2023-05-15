1a33c404956aedd3a2738574351c261ff022ad1e
blackoaz <pauloakello56@gmail.com>
2023-05-15 07:11:48 -0700

    adding args and kwargs for instatantiation

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,19 @@ from datetime import datetime
 
 class BaseModel:
     """base class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor class for basemodel"""
+        date_fmt = %Y-%m-%dT%H:%M:%S.%f
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, date_fmt)
+                else:
+                    self.__dict__[key] = val
 
     def save(self):
         self.updated_at = datetime.now()
